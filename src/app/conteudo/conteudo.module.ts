9ebea6e405683fd8bb3185ff26cee4bff3f2d39b
berdg <be_rdg@hotmail.com>
2018-04-23 17:04:15 -0300

    formulario + output

--- src/app/conteudo/conteudo.module.ts
+++ src/app/conteudo/conteudo.module.ts
@@ -3,14 +3,17 @@ import { CommonModule } from '@angular/common';
 import { ListaFilmesComponent } from './lista-filmes/lista-filmes.component';
 import { FilmesCardComponent } from './filmes-card/filmes-card.component';
 import { FilmeService } from './filme.service';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { FormPesquisaComponent } from './lista-filmes/form-pesquisa/form-pesquisa.component'
+import { FormsModule} from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
-  declarations: [ListaFilmesComponent, FilmesCardComponent],
+  declarations: [ListaFilmesComponent, FilmesCardComponent, FormPesquisaComponent],
   exports: [ ListaFilmesComponent
   ],
   providers: [FilmeService]
